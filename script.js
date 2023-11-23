fe4b7d461a120a6be3ab6e06c71213204d09ec39
Wong Ming Yeung <marcus2288@hotmail.com>
2023-11-23 20:32:37 +0800

    update script.js

--- script.js
+++ script.js
@@ -1,72 +1,85 @@
 function isEven(number) {
-  return number % 2 === 0;
+  const decimalPart = number.toString().split(".")[1];
+  const lastDigit = decimalPart
+    ? Number(decimalPart[decimalPart.length - 1])
+    : 1;
+  return lastDigit % 2 === 0;
 }
 
-function formatTimestamp(timestamp) {
-  const date = new Date(timestamp * 1000);
-  return date.toLocaleString();
+function formatNumber(number, currency) {
+  let decimalPlaces = 7;
+
+  if (currency === "BTC") {
+    decimalPlaces = 12;
+  }
+
+  const decimalNumber = Number(number).toFixed(decimalPlaces).toString();
+  return decimalNumber.replace(/\.?0+$/, "");
 }
 
-function formatNumber(number) {
-  // Convert number from scientific notation to decimal
-  const decimalNumber = Number(number).toFixed(8);
-  const formattedNumber = decimalNumber.replace(/\.?0+$/, "");
+function fetchDataAndDisplay() {
+  const baseCurrency = "HKD";
+  const apiKey = "7XDTP3CJald0cqlBLKPwjS5nJ4vXcszF";
+  const apiUrl = `https://api.apilayer.com/fixer/latest?base=${baseCurrency}`;
 
-  return formattedNumber;
-}
-
-function fetchData() {
-  fetch("https://api.apilayer.com/fixer/latest", {
+  fetch(apiUrl, {
     headers: {
-      // apikey: "9sOx2d1gWYZrKY0uD4hCbOMIQzLcL4KQ",
-      apikey: "KjEKIhHJCqXzWqQukacBKxWoTWpFcBuT",
+      apikey: apiKey,
     },
   })
     .then((response) => response.json())
     .then((data) => {
-      // Add 10.0002 to each currency value
-      const updatedData = {};
-      for (const [currency, rate] of Object.entries(data.rates)) {
-        updatedData[currency] = rate + 10.0002;
+      const originalData = data.rates;
+      const dataArray = [];
+      for (let key in originalData) {
+        dataArray.push({
+          currency: key,
+          rate: originalData[key],
+          updatedRate: originalData[key] + 10.0002,
+        });
       }
-
-      // Display data in the table
-      const tableBody = document.querySelector("#forexTable tbody");
-      tableBody.innerHTML = ""; // Clear existing rows
-
-      for (const [currency, rate] of Object.entries(data.rates)) {
-        const row = document.createElement("tr");
-        const currencyCell = document.createElement("td");
-        const rateCell = document.createElement("td");
-        const updatedRateCell = document.createElement("td");
-        const timestampCell = document.createElement("td");
-
-        currencyCell.textContent = currency;
-        rateCell.textContent = formatNumber(rate);
-        updatedRateCell.textContent = formatNumber(updatedData[currency]);
-        timestampCell.textContent = formatTimestamp(data.timestamp);
-
-        if (isEven(rate) || currency === "HKD") {
-          row.classList.add("even");
-        }
-
-        if (currency === "HKD") {
-          currencyCell.classList.add("hkd");
-          rateCell.classList.add("hkd");
-          updatedRateCell.classList.add("hkd");
-          timestampCell.classList.add("hkd");
-        }
-
-        row.appendChild(currencyCell);
-        row.appendChild(rateCell);
-        row.appendChild(updatedRateCell);
-        row.appendChild(timestampCell);
-
-        tableBody.appendChild(row);
-      }
+      displayData(dataArray, true);
     })
     .catch((error) => console.log(error));
 }
 
-fetchData();
-setInterval(fetchData, 60000);
+function displayData(data) {
+  const tableBody = document.querySelector("#forexTable tbody");
+  tableBody.innerHTML = "";
+
+  for (let item of data) {
+    const row = document.createElement("tr");
+    const currencyCell = document.createElement("td");
+    const rateCell = document.createElement("td");
+    const updatedRateCell = document.createElement("td");
+
+    const updateRate = formatNumber(item.updatedRate, item.currency);
+
+    currencyCell.textContent = item.currency;
+    rateCell.textContent = item.rate;
+    updatedRateCell.textContent = updateRate;
+
+    if (isEven(item.rate)) {
+      rateCell.classList.add("even");
+    }
+
+    if (isEven(updateRate)) {
+      updatedRateCell.classList.add("even");
+    }
+
+    if (item.currency === "HKD") {
+      row.classList.add("hkd");
+      // currencyCell.classList.add("hkd");
+      // rateCell.classList.add("hkd");
+      // updatedRateCell.classList.add("hkd");
+    }
+
+    row.appendChild(currencyCell);
+    row.appendChild(rateCell);
+    row.appendChild(updatedRateCell);
+
+    tableBody.appendChild(row);
+  }
+}
+
+fetchDataAndDisplay();
